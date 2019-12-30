ddb29c9a060ee113f6fc9cf14ac370ea5aa0dce4
Zero656329 <hwj656329@163.com>
2019-12-30 22:43:05 +0800

    多线程开发

---
+++ src/main/java/com/sunny/ifildispatcher/IfilDispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.ifildispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfilDispatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfilDispatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunny/ifildispatcher/IfilDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.ifildispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfilDispatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
